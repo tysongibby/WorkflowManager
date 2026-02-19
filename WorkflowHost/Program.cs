8232ae8c20a6a6f5bee3639d00b581020ae1b68e
Tyson Gibby <tyson.gibby@outlook.com>
2026-02-19 15:34:59 -0700

    Initial 2

--- WorkflowHost/Program.cs
+++ WorkflowHost/Program.cs
@@ -2,50 +2,74 @@ using Elsa.EntityFrameworkCore.Extensions;
 using Elsa.EntityFrameworkCore.Modules.Management;
 using Elsa.EntityFrameworkCore.Modules.Runtime;
 using Elsa.Extensions;
-//using Elsa.Studio.Host.Options;
 
 var builder = WebApplication.CreateBuilder(args);
-var config = builder.Configuration;
-var defaultConnection = config.GetConnectionString("DefaultConnection");
-if (string.IsNullOrWhiteSpace(defaultConnection)) throw new InvalidOperationException("The connection string 'DefaultConnection' was not found or is empty.");
+var configuration = builder.Configuration;
+var dbConnection = configuration.GetConnectionString("DefaultConnection") ?? throw new ArgumentNullException("dbConnection is null: Database connection string cannot be null");
 
-// Add Elsa Host services
 builder.Services.AddElsa(elsa =>
 {
-    elsa.UseWorkflowManagement(management => management.UseEntityFrameworkCore(options => options.UseSqlServer(defaultConnection)));
-    elsa.UseWorkflowRuntime(runtime => runtime.UseEntityFrameworkCore(options => options.UseSqlServer(defaultConnection)));
-    elsa.UseWorkflowsApi();
+    // Configure Management layer to use EF Core.
+    elsa.UseWorkflowManagement(management => management.UseEntityFrameworkCore(ef => ef.UseSqlServer(dbConnection)));
+
+    // Configure Runtime layer to use EF Core.
+    elsa.UseWorkflowRuntime(runtime => runtime.UseEntityFrameworkCore(ef => ef.UseSqlServer(dbConnection)));
+
+    // Default Identity features for authentication/authorization.
     elsa.UseIdentity(identity =>
     {
+        identity.TokenOptions = options => options.SigningKey = "sufficiently-large-secret-signing-key"; // This key needs to be at least 256 bits long.
         identity.UseAdminUserProvider();
-        identity.TokenOptions = options =>
-        {
-            options.SigningKey = config["Identity:SigningKey"]!;
-            options.AccessTokenLifetime = TimeSpan.FromDays(1);
-        };
     });
-    elsa.UseDefaultAuthentication();
-    elsa.UseScheduling();
+
+    // Configure ASP.NET authentication/authorization.
+    elsa.UseDefaultAuthentication(auth => auth.UseAdminApiKey());
+
+    // Expose Elsa API endpoints.
+    elsa.UseWorkflowsApi();
+
+    // Setup a SignalR hub for real-time updates from the server.
+    elsa.UseRealTimeWorkflows();
+
+    // Enable C# workflow expressions
     elsa.UseCSharp();
-    elsa.UseJavaScript();
-    elsa.UseLiquid();
+
+    // Enable JavaScript workflow expressions
+    elsa.UseJavaScript(options => options.AllowClrAccess = true);
+
+    // Enable HTTP activities.
+    elsa.UseHttp(options => options.ConfigureHttpOptions = httpOptions => httpOptions.BaseUrl = new("https://localhost:5001"));
+
+    // Use timer activities.
+    elsa.UseScheduling();
+
+    // Register custom activities from the application, if any.
+    elsa.AddActivitiesFrom<Program>();
+
+    // Register custom workflows from the application, if any.
+    elsa.AddWorkflowsFrom<Program>();
 });
 
-// Add Elsa Studio services
-//builder.Services.AddElsaStudio(elsa =>
-//{
-//    elsa.HostOptions = new ElsaHostOptions
-//    {
-//        HeadlessMode = true
-//    };
-//});
+// Configure CORS to allow designer app hosted on a different origin to invoke the APIs.
+builder.Services.AddCors(cors => cors
+    .AddDefaultPolicy(policy => policy
+        .AllowAnyOrigin() //TODO: For development purposes only. Use a specific origin for production.
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithExposedHeaders("x-elsa-workflow-instance-id"))); //TODO: Required for Elsa Studio in order to support running workflows from the designer. Alternatively, you can use the `*` wildcard to expose all headers.
 
-builder.Services.AddRazorPages();
+// Add Health Checks.
+builder.Services.AddHealthChecks();
 
+// Build the web application.
 var app = builder.Build();
-app.UseStaticFiles();
+
+// Configure web application's middleware pipeline.
+app.UseCors();
+app.UseRouting(); // Required for SignalR.
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseWorkflowsApi();
-app.MapFallbackToPage("/_Host");
+app.UseWorkflowsApi(); // Use Elsa API endpoints.
+app.UseWorkflows(); // Use Elsa middleware to handle HTTP requests mapped to HTTP Endpoint activities.
+app.UseWorkflowsSignalRHubs(); // Optional SignalR integration. Elsa Studio uses SignalR to receive real-time updates from the server. 
 app.Run();
--- WorkflowWebUi/Program.cs
+++ WorkflowWebUi/Program.cs
@@ -1,23 +1,49 @@
-﻿//using WorkflowWebUi;
+using Elsa.Studio.Contracts;
 using Elsa.Studio.Core.BlazorWasm.Extensions;
+using Elsa.Studio.Dashboard.Extensions;
 using Elsa.Studio.Extensions;
 using Elsa.Studio.Login.BlazorWasm.Extensions;
+using Elsa.Studio.Login.Extensions;
 using Elsa.Studio.Login.HttpMessageHandlers;
+using Elsa.Studio.Models;
 using Elsa.Studio.Shell;
 using Elsa.Studio.Shell.Extensions;
 using Elsa.Studio.Workflows.Designer.Extensions;
+using Elsa.Studio.Workflows.Extensions;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+// Build the host.
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+//var configuration = builder.Configuration;
+
+// Register root components.
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.RootComponents.RegisterCustomElsaStudioElements();
+
+// Register shell services and modules.
+var backendApiConfig = new BackendApiConfig
+{
+    ConfigureBackendOptions = options => builder.Configuration.GetSection("Backend").Bind(options),
+    ConfigureHttpClientBuilder = options => options.AuthenticationHandler = typeof(AuthenticatingApiHttpMessageHandler)
+};
 
 builder.Services.AddCore();
 builder.Services.AddShell();
-//builder.Services.AddRemoteBackend( options => options.AuthenticationHandler = typeof(AuthenticatingApiHttpMessageHandler));
-builder.Services.AddHttpMessageHandler<AuthenticatingApiHttpMessageHandler>();
+builder.Services.AddRemoteBackend(backendApiConfig);
 builder.Services.AddLoginModule();
-builder.Services.AddWorkflowsDesigner();
+builder.Services.UseElsaIdentity();
+builder.Services.AddDashboardModule();
+builder.Services.AddWorkflowsModule();
 
-await builder.Build().RunAsync();
+
+// Build the application.
+var app = builder.Build();
+
+// Run startup tasks.
+var startupTaskRunner = app.Services.GetRequiredService<IStartupTaskRunner>();
+await startupTaskRunner.RunStartupTasksAsync();
+
+// Run the application.
+await app.RunAsync();
